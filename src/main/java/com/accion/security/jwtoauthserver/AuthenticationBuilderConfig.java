d99d5c356de7fa4a96f7babc45d762fe0cdf9cc1
Venkatesh <venkatesh.sarvepalli@gmail.com>
2018-02-22 17:21:18 +0530

    -- Ldap Integration.

--- src/main/java/com/accion/security/jwtoauthserver/AuthenticationBuilderConfig.java
+++ src/main/java/com/accion/security/jwtoauthserver/AuthenticationBuilderConfig.java
@@ -3,11 +3,12 @@ package com.accion.security.jwtoauthserver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import com.accion.security.jwtoauthserver.authBuilder.AuthenticationType;
 
-//@Configuration
+@Configuration
 @EnableConfigurationProperties
 public class AuthenticationBuilderConfig {
     @Value("${authentication.AuthenticationBuilder}")
